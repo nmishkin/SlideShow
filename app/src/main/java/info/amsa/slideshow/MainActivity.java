7e658c56cfee2d4bec294d4fc21805791e92312b
Nathaniel Mishkin <mishkin@aya.yale.edu>
2017-01-21 23:16:25 -0500

    WIP

--- app/src/main/java/info/amsa/slideshow/MainActivity.java
+++ app/src/main/java/info/amsa/slideshow/MainActivity.java
@@ -1,56 +1,111 @@
 package info.amsa.slideshow;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private ImageView imageView;
     private File filesDir;
     private Random random = new Random();
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView) findViewById(R.id.imageView);
 
-        filesDir = getFilesDir();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        }
+        //if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        //    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        //}
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        File picturesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        filesDir = new File(picturesDir, "Photo Frame");
         if (!filesDir.exists()) {
             return;
         }
 
-        new PhotoLoaderTask().execute();
+        screenOn();
 
+        new PhotoLoaderTask().execute();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    }
+    private void screenOn() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        runAtNextHourMinute(11, 30, new Runnable() {
+            @Override
+            public void run() {
+                screenOff();
+            }
+        });
+    }
+    private void screenOff() {
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        runAtNextHourMinute(6, 30, new Runnable() {
+            @Override
+            public void run() {
+                screenOn();
+            }
+        });
+    }
+
+    private void runAtNextHourMinute(int hour, int minute, Runnable runnable) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, hour);
+        cal.set(Calendar.MINUTE, minute);
+        if (cal.before(Calendar.getInstance())) {
+            cal.add(Calendar.HOUR_OF_DAY, 24);
+        }
+        long intervalToNextSleepTime = cal.getTimeInMillis() - System.currentTimeMillis();
+        long postTime = SystemClock.uptimeMillis() + intervalToNextSleepTime;
+        handler.postAtTime(runnable, postTime);
     }
 
     private class PhotoLoaderTask extends AsyncTask<Void, Void, Bitmap> {
-
-
         @Override
         protected Bitmap doInBackground(Void[] params) {
             List<File> imageFiles = new ArrayList<>();
-            for (File imageFile : filesDir.listFiles()) {
+            File[] fileList = filesDir.listFiles();
+            if (fileList == null) {
+                return null;
+            }
+
+            for (File imageFile : fileList) {
                 if (!imageFile.isFile() || !imageFile.getPath().toLowerCase().endsWith(".jpg")) {
                     continue;
                 }
                 imageFiles.add(imageFile);
             }
 
+            if (imageFiles.isEmpty()) {
+                return null;
+            }
             int index = Math.abs(random.nextInt()) % imageFiles.size();
             File imageFile = imageFiles.get(index);
             Bitmap bm = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
@@ -61,13 +116,14 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-
             return bm;
         }
 
         @Override
         protected void onPostExecute(Bitmap bm) {
-            imageView.setImageBitmap(bm);
+            if (bm != null) {
+                imageView.setImageBitmap(bm);
+            }
             new PhotoLoaderTask().execute();
         }
     }
